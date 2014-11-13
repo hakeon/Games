b17246446dc03fe7e55248ddc6792e168ce8e7e4
HAKEON <hakeon@gmail.com>
2014-11-12 23:08:02 -0500

    migrate database

--- app/config/database.php
+++ app/config/database.php
@@ -55,9 +55,9 @@ return array(
 		'mysql' => array(
 			'driver'    => 'mysql',
 			'host'      => 'localhost',
-			'database'  => 'forge',
-			'username'  => 'forge',
-			'password'  => '',
+			'database'  => 'games',
+			'username'  => 'root',
+			'password'  => 'Malina01',
 			'charset'   => 'utf8',
 			'collation' => 'utf8_unicode_ci',
 			'prefix'    => '',
